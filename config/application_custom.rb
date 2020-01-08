9d3c34d71ab804da8c655b820b7aa31c2c8deb29
Francesc Domènech <francesc.domenech@canals.es>
2020-01-08 17:20:55 +0100

    .

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,6 +1,6 @@
 module Consul
   class Application < Rails::Application
-    config.i18n.default_locale = :va
-    config.i18n.available_locales = [:va, :es]
+    config.i18n.default_locale = :val
+    config.i18n.available_locales = [:val, :es]
   end
 end
